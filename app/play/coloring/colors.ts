73857049291bc8d4436c3f86ef4de64bb8a0ddfd
Abinash Sethi <sethiabinash123@gmail.com>
2026-01-19 16:53:09 +0530

    Move app source and Dockerfile into Git

---
+++ app/play/coloring/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = [
+  "#FADADD", // peach
+  "#FFF1A8", // soft yellow
+  "#BEE7E8", // sky blue
+  "#CDEAC0", // mint green
+  "#E6C7FF", // lavender
+  "#D7B899", // light brown
+  "#FFFFFF", // eraser
+];
